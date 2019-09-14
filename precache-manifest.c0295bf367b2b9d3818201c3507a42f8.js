0a9dee0417b0e89d66f698a3bca30c7f6a85aaeb
RobertDruska <robert.druska@gmail.com>
2019-09-14 23:15:12 +0200

    init

---
+++ precache-manifest.c0295bf367b2b9d3818201c3507a42f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f307eedcd1b362b92d31c876ecc1eff2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38848c57f1c857eaee3f",
+    "url": "/static/css/main.0687d564.chunk.css"
+  },
+  {
+    "revision": "4a70b000e92d4c847499",
+    "url": "/static/js/2.092fef90.chunk.js"
+  },
+  {
+    "revision": "38848c57f1c857eaee3f",
+    "url": "/static/js/main.d5a1a488.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
